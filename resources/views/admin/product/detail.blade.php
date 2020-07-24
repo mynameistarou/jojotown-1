a168ee49afe5da68ea1504e1c4bd94a39a185598
EC2 Default User <ec2-user@ip-172-31-12-212.ap-northeast-1.compute.internal>
2020-07-24 10:25:33 +0000

    編集画面を変更

--- resources/views/admin/product/detail.blade.php
+++ resources/views/admin/product/detail.blade.php
@@ -1,34 +1,60 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>商品追加フォーム</title>
-    </head>
-    <body>
-        <h1>商品追加フォーム</h1>
-        <form action="{{ $action }}" method="post">
-            @csrf
-            <div>
-                <label for="product_name">商品名</label>
-                <input type="text" id="product_name" name="product_name">
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">商品追加フォーム</div>
+                <form class="mt-3" method="POST" action="{{ $action }}">
+                    @csrf
+                    
+                    <div class="form-group row">
+                        <label for="product_name" class="col-md-4 col-form-label text-md-right">商品名</label>
+                        <div class="col-md-6">
+                            <input id="product_name" class="form-control" name="product_name" value="{{ old('product_name') }}" required>
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="product_explain" class="col-md-4 col-form-label text-md-right">商品説明</label>
+                        <div class="col-md-6">
+                            <textarea id="product_explain" class="form-control" name="product_explain" value="{{ old('product_explain') }}"></textarea>
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="price" class="col-md-4 col-form-label text-md-right">値段（円）</label>
+                        <div class="col-md-6">
+                            <input type="number" id="price" class="form-control" name="price" value="{{ old('price') }}">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="url" class="col-md-4 col-form-label text-md-right">URL</label>
+                        <div class="col-md-6">
+                            <input id="url" class="form-control" name="url" value="{{ old('url') }}">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="image_url" class="col-md-4 col-form-label text-md-right">画像URL</label>
+                        <div class="col-md-6">
+                            <input id="image_url" class="form-control" name="image_url" value="{{ old('image_url') }}">
+                        </div>
+                    </div>
+
+                    <div class="form-group row mb-3">
+                        <div class="col-md-8 offset-md-4">
+                            <button type="submit" class="btn btn-primary">
+                                送信
+                            </button>
+                            <a href="/admin/product">商品一覧</a>
+                        </div>
+                    </div>
+                </form>
             </div>
-            <div>
-                <label for="product_explain">商品説明<label>
-                <textarea id="product_explain" name="product_explain"></textarea>
-            </div>
-            <div>
-                <label for="price">値段</label>
-                <input type="text"　id="price" name="price">
-            </div>
-            <div>
-                <label for="url">URL</label>
-                <input type="text"　id="url" name="url">
-            </div>
-            <div>
-                <label for="image_url">画像URL</label>
-                <input type="image_url"　id="image_url" name="image_url">
-            </div>
-            <input type="submit" value="送信">
-        </form>
-        <a href="/product">商品一覧</a>
-    </body>
-</html>
+        </div>
+    </div>
+</div>
+@endsection
